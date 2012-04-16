7607cb7c3721613eced645c80556c4c7121ce73b
Victor Borja <vborja@apache.org>
2012-04-16 11:10:46 -0500

    License

--- color-theme-buffer-local.el
+++ color-theme-buffer-local.el
@@ -3,7 +3,25 @@
 ;;; Author: Victor Borja <vic.borja@gmail.com>
 ;;; URL: http://github.com/vic/color-theme-buffer-local
 ;;; Description: Set color-theme by buffer.
-;;; 
+;;; Keywords: faces
+
+
+;; This file is not part of GNU Emacs. Licensed on the same terms as
+;; GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
 
 ;;;
 ;;; Usage for color-theme.el themes:
